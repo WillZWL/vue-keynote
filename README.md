b1ac4c1b89f989b7e66bf6df3366cad191aa7b51
Will <will.zhang@eservicesgroup.com>
2017-06-04 23:34:15 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # vue-keynote
 > vue-keynote
 
+## Description
+用Vue写的一个分享Vue的PPT，在写PPT的时候把分享中要用到的Demo也写出了，在写Demo的时候顺带把分享要用到的PPT给写了
+
 ## Build Setup
 
 ``` bash
--- src/pages/Build-tool.vue
+++ src/pages/Build-tool.vue
@@ -11,12 +11,12 @@
       <pre>vue list</pre>
 
       <ol>
-          <li>browserify - A full-featured Browserify + vueify setup with hot-reload, linting & unit testing.</li>
-          <li>browserify-simple - A simple Browserify + vueify setup for quick prototyping.</li>
-          <li>pwa - PWA template for vue-cli based on the webpack template</li>
-          <li>simple - The simplest possible Vue setup in a single HTML file</li>
-          <li>webpack - A full-featured Webpack + vue-loader setup with hot reload, linting, testing & css extraction.</li>
-          <li>webpack-simple - A simple Webpack + vue-loader setup for quick prototyping.</li>
+        <li>browserify - A full-featured Browserify + vueify setup with hot-reload, linting & unit testing.</li>
+        <li>browserify-simple - A simple Browserify + vueify setup for quick prototyping.</li>
+        <li>pwa - PWA template for vue-cli based on the webpack template</li>
+        <li>simple - The simplest possible Vue setup in a single HTML file</li>
+        <li>webpack - A full-featured Webpack + vue-loader setup with hot reload, linting, testing & css extraction.</li>
+        <li>webpack-simple - A simple Webpack + vue-loader setup for quick prototyping.</li>
       </ol>
     </v-wrap>
   </box>
